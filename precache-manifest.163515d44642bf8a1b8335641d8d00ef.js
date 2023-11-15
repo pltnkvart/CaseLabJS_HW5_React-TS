16d4780b7a4810da2ae177b3a2cab4495969342e
pltnkvart <artosi@yandex.ru>
2023-11-15 14:07:12 +0300

    Updates

---
+++ precache-manifest.163515d44642bf8a1b8335641d8d00ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf0650f3875220dc7d141df0fbe0091",
+    "url": "/CaseLabJS_HW5_React-TS/index.html"
+  },
+  {
+    "revision": "a88a405ae926743d59e2",
+    "url": "/CaseLabJS_HW5_React-TS/static/js/2.11143aa2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CaseLabJS_HW5_React-TS/static/js/2.11143aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1413c02c0eb438b5883f",
+    "url": "/CaseLabJS_HW5_React-TS/static/js/main.275e8e47.chunk.js"
+  },
+  {
+    "revision": "3a90482743e67f8441da",
+    "url": "/CaseLabJS_HW5_React-TS/static/js/runtime-main.1ac1bf5c.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/CaseLabJS_HW5_React-TS/static/media/font.f01e8d15.woff"
+  }
+]);
